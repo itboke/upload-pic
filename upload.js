0c4de58d1147b7704d7191054a2617da90ee29c9
huangzelin <hzl@xxx.com>
2016-10-12 18:43:55 +0800

    up

--- upload.js
+++ upload.js
@@ -20,95 +20,51 @@
             _SELF._file.addEventListener('change',function(){
                 if (!this.files.length) return;
                 var _files = Array.prototype.slice.call(this.files);
+                var _name  = this.getAttribute('name');
                 _files.forEach(function(file,i){
-                    //http://blog.csdn.net/zk437092645/article/details/8745647 [TML5学习之FileReader接口]
-                    var _reader = new FileReader();
-                    //将文件读取为DataURL
-                    _reader.readAsDataURL(file);
-                    _reader.onload = function(){
-                        var _params = _SELF._dataSet(_SELF._file,_SELF);
-                        var _result = this.result;
-                        _SELF._upload(_params,_result,file.type);
+                    var _isType = _SELF._checkFileType(file.type);
+                    if(!_isType){
+                        alert('上传的文件类型有误');
+                        return false;
                     }
-
+                    var _data = file;
+                    _SELF._upload(_data,_name);
                 })
             });
         },
-        //上传到服务器
-        _upload:function(obj,_result,type){
-            var _SELF = this,
-                _data = {};
-
-            for (var key in obj) {
-                if (key !== 'luploader') {
-                    if (obj[key] == 'basestr') {
-                        _data[key] = _result;
-                    } else {
-                        _data[key] = obj[key];
-                    }
+        //判断文件类型
+        _checkFileType:function(_type){
+            var _allowType = ['image/jpeg','image/png','image/gif'];
+            for(var i=0,l = _allowType.length; i<l; i++)
+            {
+                if(_allowType[i] === _type){
+                    return true;
                 }
-            };
-            _data =_SELF._serializeObject(_data);
+            }
+            return false;
+        },
+        //上传到服务器
+        _upload:function(option,name){
+            var _SELF = this;
+            var formData = new FormData();
+            formData.append(name,option);
             //创建ajax
             var _xhr = new XMLHttpRequest();
             //建立请求
-            _xhr.open('post','http://localhost/upload/upload.php',true);
-            _xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            _xhr.open('post','http://localhost/upload-pic/upload.php',true);
             _xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
             //发送请求
-            _data?_xhr.send(_data):_xhr.send()
+            formData?_xhr.send(formData):_xhr.send()
             //请求回答
             _xhr.onreadystatechange = function(){
                 if(_xhr.readyState == 4){
                     if(_xhr.status == 200){
-                        console.log(_xhr.responseText)
+                        alert(_xhr.responseText)
                     }else{
-                        console.log('fault')
+                        alert('上传失败');
                     }
                 }
             }
-        },
-        //序列化
-        _serializeObject:function(obj){
-            if (typeof obj === 'string') return obj;
-            var resultArray = [];
-            var separator = '&';
-            var isArray  = function(arr){
-                if (Object.prototype.toString.apply(arr) === '[object Array]') return true;
-                else return false;
-            };
-            for (var prop in obj) {
-                if (obj.hasOwnProperty(prop)) {
-                    if (isArray(obj[prop])) {
-                        var toPush = [];
-                        for (var i = 0; i < obj[prop].length; i++) {
-                            toPush.push(encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop][i]));
-                        }
-                        if (toPush.length > 0) resultArray.push(toPush.join(separator));
-                    } else {
-                        resultArray.push(encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop]));
-                    }
-                }
-
-            }
-            return resultArray.join(separator);
-        },
-        //获取el属性
-        _dataSet:function(el,_this){
-            var _SELF   = _this;
-            var dataset = {};
-            for (var i = 0; i < el.attributes.length; i++) {
-                var attr = el.attributes[i];
-                if (attr.name.indexOf('data-') >= 0) {
-                    dataset[_SELF._toCamelCase(attr.name.split('data-')[1])] = attr.value;
-                }
-            }
-            return dataset;
-        },
-        _toCamelCase:function(string){
-            return string.toLowerCase().replace(/-(.)/g, function(match, group1) {
-                return group1.toUpperCase();
-            });
         },
         //复制参数对象
         _copy:function(opts){
--- upload.php
+++ upload.php
@@ -1,14 +1,10 @@
 <?php
 
-    //get the content with base64'style of the upload-picture
-    $base64_image_content = $_POST['formFile'];
-    //add the pic to driver
-    if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)){
-        $type = $result[2];
-        $new_file = "./test.{$type}";
-        if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))){
-            echo '新文件保存成功：', $new_file;
-        }
+    //保存图片
+    if(file_exists($_FILES["file"]["name"])){
+        echo $_FILES["file"]["name"].'already exists';
+    }else{
+        move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
+        echo '上传成功';
     }
-
 ?>
